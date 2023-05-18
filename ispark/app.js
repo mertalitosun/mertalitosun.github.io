03dbf341865a5c105de82726966b025bcea02b4f
mertalit <mertali2635@icloud.com>
2023-05-18 16:16:21 +0300

    .

--- ispark/app.js
+++
@@ -1,6 +0,0 @@
-const body = document.querySelector("body");
-const buton = document.querySelector(".buton");
-const box = document.querySelector(".box");
-const footer = document.querySelector(".footer");
-
-
