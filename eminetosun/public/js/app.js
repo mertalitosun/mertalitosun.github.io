603ff497dbb719447ed560ed71154c1d29edc3b2
Mertali Tosun <mertali2635@icloud.com>
2024-01-02 22:12:07 +0300

    digitalEvent

---
+++ eminetosun/public/js/app.js
@@ -0,0 +1,31 @@
+const day = document.getElementById("day");
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+function updateTime() {
+  const now = new Date().getTime();
+  console.log(now);
+
+  const endDate = new Date(2024, 1, 24, 20, 0, 0).getTime();
+  console.log(endDate);
+
+  const remainingTime = endDate - now;
+  console.log(remainingTime);
+
+  if (remainingTime > 0) {
+    const seconds = remainingTime / 1000;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+
+    day.innerHTML = `${parseInt(days)}`;
+    hour.innerHTML = `${parseInt(hours % 24)}`;
+    min.innerHTML = `${parseInt(minutes % 60)}`;
+    sec.innerHTML = `${parseInt(seconds % 60)}`;
+    setTimeout(updateTime,1000)
+  }else{
+    day.innerHTML="bitti"
+  }
+}
+updateTime();
