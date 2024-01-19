e9ffbf3652b2b4b57c88c1abcaa5b922b5574424
mertalitosun <mertali2635@icloud.com>
2024-01-19 17:20:37 +0300

    Update audio.js

--- neslihanonur-1/public/js/audio.js
+++ neslihanonur-1/public/js/audio.js
@@ -1,9 +1,5 @@
 const audioButton = document.getElementById("audioButton");
 const audio = document.getElementById("audio");
-
-window.addEventListener("scroll",()=>{
-    audio.play();
-})
 // audio play/pause
 audioButton.addEventListener("click", () => {
   if (audio.paused) {
