989bee9a6b7b60f49ed091426be36911ac789a22
mertalitosun <mertali2635@icloud.com>
2024-01-19 16:01:50 +0300

    updated audio

--- neslihanonur-1/public/js/app.js
+++ neslihanonur-1/public/js/app.js
@@ -1,18 +1,4 @@
 const touchIcon = document.getElementById("touchIcon");
-const audioButton = document.getElementById("audioButton");
-const audio = document.getElementById("audio");
-
-
-// audio play/pause
-audioButton.addEventListener("click",()=>{
-  if(audio.paused){
-    audio.play();
-  }else{
-    audio.pause();
-  }
-})
-
-
 setInterval(() => {
   if (touchIcon.style.display == "block") {
     touchIcon.style.display = "none";
